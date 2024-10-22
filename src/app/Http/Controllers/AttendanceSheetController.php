2cd7e940368a5c26fc3beda5a2aa3d1ac4f72b5d
Mana Fukumoto <mana66354@gmail.com>
2024-10-22 09:55:51 +0900

    コントローラの修正

--- src/app/Http/Controllers/AttendanceSheetController.php
+++ src/app/Http/Controllers/AttendanceSheetController.php
@@ -5,24 +5,34 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\AttendanceSheet;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Notifications\Notifiable;
 
 class AttendanceSheetController extends Controller
 {
     use Notifiable;
 
-    public function index()
+    //日付一覧の表示
+    public function index(Request $request)
     {
-        $users = User::with([
-            'attendanceSheets' => function ($query) {
-                $query->orderBy('start_time', 'desc')->limit(1);
-            }
-        ])->paginate(5);
 
-        return view('attendance-sheet', compact('users'));
+        $date = $request->input('date', Carbon::today('Asia/Tokyo'));
+
+        $users = User::whereHas('attendanceSheets', function ($query) use ($date) {
+            $query->whereDate('start_time', $date->format('Y-m-d'));
+        })
+            ->with([
+                'attendanceSheets' => function ($query) use ($date) {
+                    $query->whereDate('start_time', $date->format('Y-m-d'))
+                        ->orderBy('start_time', 'desc');
+                }
+            ])
+            ->paginate(5);
+
+        return view('attendance-sheet', compact('users', 'date'));
     }
 
-    //日付
+    //日付の前後
     public function show($date)
     {
         $date = Carbon::parse($date)->setTimezone('Asia/Tokyo');
@@ -42,41 +52,41 @@ class AttendanceSheetController extends Controller
     }
 
     public function total($id)
-{
-    $attendance = AttendanceSheet::with('breakTimes')->find($id);
+    {
+        $attendance = AttendanceSheet::with('breakTimes')->find($id);
 
-    if ($attendance && $attendance->start_time && $attendance->finish_time) { //勤務開始・終了が存在する場合
-        //勤務時間の計算
-        $startTime = Carbon::parse($attendance->start_time);
-        $finishTime = Carbon::parse($attendance->finish_time);
-        $totalWorkMinutes = $finishTime->diffInMinutes($startTime);
+        if ($attendance && $attendance->start_time && $attendance->finish_time) { //勤務開始・終了が存在する場合
+            //勤務時間の計算
+            $startTime = Carbon::parse($attendance->start_time);
+            $finishTime = Carbon::parse($attendance->finish_time);
+            $totalWorkMinutes = $finishTime->diffInMinutes($startTime);
 
-        //休憩時間の合計の計算
-        $totalBreakMinutes = $attendance->breakTimes->reduce(function ($carry, $breakTime) {
-            //休憩終了時間が存在するかチェック
-            if ($breakTime->finish_break) {
-                $startBreak = Carbon::parse($breakTime->start_break);
-                $finishBreak = Carbon::parse($breakTime->finish_break);
-                return $carry + $finishBreak->diffInMinutes($startBreak);
-            }
-            return $carry;
-        }, 0);
+            //休憩時間の合計の計算
+            $totalBreakMinutes = $attendance->breakTimes->reduce(function ($carry, $breakTime) {
+                //休憩終了時間が存在するかチェック
+                if ($breakTime->finish_break) {
+                    $startBreak = Carbon::parse($breakTime->start_break);
+                    $finishBreak = Carbon::parse($breakTime->finish_break);
+                    return $carry + $finishBreak->diffInMinutes($startBreak);
+                }
+                return $carry;
+            }, 0);
 
-        //実働時間 = 勤務時間 - 休憩時間
-        $actualWorkMinutes = $totalWorkMinutes - $totalBreakMinutes;
+            //実働時間 = 勤務時間 - 休憩時間
+            $actualWorkMinutes = $totalWorkMinutes - $totalBreakMinutes;
 
-        return view('attendance.total', [
-            'attendance' => $attendance,
-            'totalWorkHours' => floor($totalWorkMinutes / 60),
-            'totalWorkMinutes' => $totalWorkMinutes % 60,
-            'totalBreakHours' => floor($totalBreakMinutes / 60),
-            'totalBreakMinutes' => $totalBreakMinutes % 60,
-            'actualWorkHours' => floor($actualWorkMinutes / 60),
-            'actualWorkMinutes' => $actualWorkMinutes % 60,
-        ]);
+            return view('attendance.total', [
+                'attendance' => $attendance,
+                'totalWorkHours' => floor($totalWorkMinutes / 60),
+                'totalWorkMinutes' => $totalWorkMinutes % 60,
+                'totalBreakHours' => floor($totalBreakMinutes / 60),
+                'totalBreakMinutes' => $totalBreakMinutes % 60,
+                'actualWorkHours' => floor($actualWorkMinutes / 60),
+                'actualWorkMinutes' => $actualWorkMinutes % 60,
+            ]);
+        }
+
+        //勤務データが存在しない場合
+        return redirect()->back();
     }
-
-    //勤務データが存在しない場合
-    return redirect()->back();
-}
 }
